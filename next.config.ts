d5fd1cdee77f1a3cf6c9bcbbeccd21073fd5538a
Zeke-lab <yemyatmin192@gmail.com>
2026-09-21 16:29:16 +0700

    change assignment file and next js to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Learning_Space",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
